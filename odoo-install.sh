e9e29ccc8bb4c61e5508045106a1b94bca9b0d30
Kevin Kong <kfx2007@outlook.com>
2023-11-25 21:09:19 +0800

    [IMP]Ubuntu 22.04 0.12.5

--- odoo-install.sh
+++ odoo-install.sh
@@ -5,11 +5,13 @@ read -r -p  "请输入要安装的版本:" version
 echo "开始安装odoo服务.."
 wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 echo "deb http://nightly.odoocdn.com/$version/nightly/deb/ ./" >> /etc/apt/sources.list
-apt-get update && apt-get -y install odoo
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+apt-get update && apt-get -y install libssl1.1 xfonts-75dpi odoo
 
 echo "开始安装wkhtmptopdf.."
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb
 dpkg -i wkhtmltox_0.12.5-1.focal_amd64.deb
+
 echo "开始安装中文字体.."
 apt-get -y install ttf-wqy-microhei ttf-wqy-zenhei
 
