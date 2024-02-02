1f78380c90e84cd1a24319c1ebac030809b60892
Kevin <6907475+jellyfrank@users.noreply.github.com>
2024-02-02 08:54:22 +0800

    Update wkhtml.sh

--- wkhtml.sh
+++ wkhtml.sh
@@ -3,4 +3,6 @@ cd ~
 # wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 # tar xvf wkhtmltox*.tar.xz
 # sudo mv wkhtmltox/bin/wkhtmlto* /usr/bin
-sudo apt-get install -y openssl build-essential libssl-dev libxrender-dev git-core libx11-dev libxext-dev libfontconfig1-dev libfreetype6-dev fontconfig
+# sudo apt-get install -y openssl build-essential libssl-dev libxrender-dev git-core libx11-dev libxext-dev libfontconfig1-dev libfreetype6-dev fontconfig
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb
+dpkg -i wkhtmltox_0.12.5-1.focal_amd64.deb
